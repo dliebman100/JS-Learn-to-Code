63c446967c0cdfef62b99274a43915594cdb340f
Daniel Liebman <dliebman100@gmail.com>
2021-06-09 20:30:53 -0400

    fixed arrow syntax

--- Intro-API-AJAX/trivia-game/script.js
+++ Intro-API-AJAX/trivia-game/script.js
@@ -6,12 +6,12 @@ window.onload = function () {
     //API is passed as a fetch method, 'chaining' with .then() method is going to accept a callback function
     //This is the method for API Call (Asynchronous code)
     fetch("http://jservice.io/api/random")
-        .then((response) => {
+        .then(response => {
             console.log("success", response);
             return response.json(); //parse the Response object   
         })
         //response will become readable to us using the data, but if you want to search for clues change "data" to "clues"
-        .then((data) => {
+        .then(data => {
             console.log("random data", data[0].answer);
             //this is where you do your DOM manipulation logic
 
@@ -38,7 +38,7 @@ window.onload = function () {
             categoryElement.innerText = `Category: ${category}`;
             questionContainer.append(categoryElement);
         })
-        .catch((error) => {
+        .catch(error => {
             console.log("error", error);
             const questionContainer = document.querySelector(".js-question-container");
             //this is where you display some error messages
@@ -48,11 +48,11 @@ window.onload = function () {
             questionContainer.append(errorElement);
         });
     fetch("http://jservice.io/api/clues") // GET request
-        .then((response) => response.json()) // simplified arrow syntax
-        .then((data) => {
+        .then(response => response.json()) // simplified arrow syntax
+        .then(data => {
             console.log("clues data", data);
         })
-        .catch((error) => console.log(error));
+        .catch(error => console.log(error));
 
     // Example of synchronous code
     // console.log("another hello world"); // this got logged second
@@ -72,9 +72,9 @@ function onQuestionsFormSubmit(event) {
 
     const userInput = document.querySelector(".js-questions-form-input").value;
 
-    fetch(`http://jservice.io/api/random?count=${userInput || 10}`) // GET request
-        .then((response) => response.json()) // simplified arrow syntax
-        .then((data) => {
+    fetch(`http://jservice.io/api/random?count=${userInput || 10}`) // GET request and add the URL
+        .then(response => response.json()) //process the data aka the json  
+        .then(data => {
             console.log("onQuestionsFormSubmit data", data);
 
             //once we get back data from API we manipulate the DOM to display the results within the unordered list
@@ -82,17 +82,18 @@ function onQuestionsFormSubmit(event) {
 
             const questionsList = document.querySelector(".js-questions-list");
             questionsFormHeading.innerText = `${data.length} Random Trivia Questions`;
-
             questionsList.innerText = "";
 
-            for(let index = 0; index < data.length; i++){
+            for (let index = 0; index < data.length; index++) {
                 //store the question in the variable question
                 const question = data[index].question;
                 const questionListItem = document.createElement("li");
                 questionListItem.innerText = `Question ${ index + 1 }: ${question}`;
+                questionsList.append(questionListItem);
             }
             // refactor the for loop ^^ to use forEach()
 
+
         })
-        .catch((error) => console.log(error));
+        .catch(error => console.log(error));
 }
